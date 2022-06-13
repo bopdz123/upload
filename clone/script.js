0fc5a564157677ccc21a40f522cd46505056f913
Linhh <bongcute270@gmail.com>
2022-06-13 15:20:59 +0700

    upload

---
+++ clone/script.js
@@ -0,0 +1,7 @@
+let pass = "cvcualinh123";
+let answer = prompt("Enter password");
+
+while (answer != pass) {
+  alert("Sai mật khẩu ! Mời bạn nhập lại!");
+  answer = prompt("Re-enter password");
+}
